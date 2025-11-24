3f2ad544a547b398a38d6d2fc36ab5f9d6d93316
Giova <giovanni.rocci@uzh.ch>
2025-11-24 15:23:01 +0100

    refactor extract_means to return detailed statistics and update LaTeX formatting

--- format_thr_clf_stats.py
+++ format_thr_clf_stats.py
@@ -1,6 +1,5 @@
-from pylatex import Document, Section, Tabular, NoEscape, Package
+from pylatex import Document, Section, Tabular, NoEscape, Package, MultiColumn
 import json
-import re
 import numpy as np
 import argparse
 import os
@@ -18,19 +17,47 @@ def extract_means(res_dir: str):
                 data = json.load(f)
                 for stat in data:
                     if stat == "threshold_f1":
-                        thr_f1.append(data[stat]["overall_mean"])
+                        thr_f1.append({"clfy":data[stat]["overall_classify"], 
+                                       "min":data[stat]["overall_minimize"], 
+                                       "max":data[stat]["overall_maximize"], 
+                                       "mean":data[stat]["overall_mean"]})
                     elif stat == "threshold_error":
-                        thr_err.append(data[stat]["overall_mean"])
+                        thr_err.append({"clfy":data[stat]["overall_classify"], 
+                                        "min":data[stat]["overall_minimize"], 
+                                        "max":data[stat]["overall_maximize"], 
+                                        "mean":data[stat]["overall_mean"]})
                     elif stat == "classifier_f1":
-                        clf_f1.append(data[stat]["overall_mean"])
+                        clf_f1.append({"clfy":data[stat]["overall_classify"], 
+                                       "min":data[stat]["overall_minimize"],
+                                       "max":data[stat]["overall_maximize"], 
+                                       "mean":data[stat]["overall_mean"]})
                     elif stat == "classifier_error":
-                        clf_err.append(data[stat]["overall_mean"])
+                        clf_err.append({"clfy":data[stat]["overall_classify"], 
+                                        "min":data[stat]["overall_minimize"], 
+                                        "max":data[stat]["overall_maximize"], 
+                                        "mean":data[stat]["overall_mean"]})
                     else:
                         continue
     
     return {
-        "threshold": {"f1" :np.mean(thr_f1), "error": np.mean(thr_err)},
-        "classifier": {"f1": np.mean(clf_f1), "error": np.mean(clf_err)},
+        "threshold": {"f1_clfy" :np.mean([d["clfy"] for d in thr_f1]),
+                        "f1_min" :np.mean([d["min"] for d in thr_f1]),
+                        "f1_max" :np.mean([d["max"] for d in thr_f1]),
+                        "f1_mean" :np.mean([d["mean"] for d in thr_f1]),
+                        "error_clfy" :np.mean([d["clfy"] for d in thr_err]),
+                        "error_min" :np.mean([d["min"] for d in thr_err]),
+                        "error_max" :np.mean([d["max"] for d in thr_err]),
+                        "error_mean" :np.mean([d["mean"] for d in thr_err])
+                      },
+        "classifier": {"f1_clfy" :np.mean([d["clfy"] for d in clf_f1]),
+                        "f1_min" :np.mean([d["min"] for d in clf_f1]),   
+                        "f1_max" :np.mean([d["max"] for d in clf_f1]),   
+                        "f1_mean" :np.mean([d["mean"] for d in clf_f1]),
+                        "error_clfy" :np.mean([d["clfy"] for d in clf_err]),
+                        "error_min" :np.mean([d["min"] for d in clf_err]),
+                        "error_max" :np.mean([d["max"] for d in clf_err]),
+                        "error_mean" :np.mean([d["mean"] for d in clf_err])
+                      }
     }
 
 
@@ -40,11 +67,22 @@ def format_stats_to_latex(stats: dict, output_path: str):
 
     with doc.create(Section('Classification Statistics')):
         doc.append(NoEscape(r'\centering'))
-        with doc.create(Tabular('l|r r')) as table:
+        with doc.create(Tabular('l|r r r r r r r r')) as table:
             table.append(NoEscape(r'\toprule'))
             table.add_row((
                 'Calibration Method',
+                MultiColumn(4, align='c', data='F1 Score'),
+                MultiColumn(4, align='c', data='Error Rate'),
+            ))
+            table.add_row((
+                '',
+                NoEscape(r'$\overline{Clfy}$'),
+                NoEscape(r'$\overline{Min}$'),
+                NoEscape(r'$\overline{Max}$'),
                 NoEscape(r'$\overline{F1}$'),
+                NoEscape(r'$\overline{Clfy}$'),
+                NoEscape(r'$\overline{Min}$'),
+                NoEscape(r'$\overline{Max}$'),
                 NoEscape(r'$\overline{Err}$'),
             ))
             table.append(NoEscape(r'\midrule'))
@@ -52,13 +90,19 @@ def format_stats_to_latex(stats: dict, output_path: str):
             for calibr_name in stats:
                 table.add_row((
                     calibr_name.capitalize(),
-                    f"{stats[calibr_name]['f1']:.4f}",
-                    f"{stats[calibr_name]['error']:.4f}",
+                    f"{stats[calibr_name]['f1_clfy']:.2f}",
+                    f"{stats[calibr_name]['f1_min']:.2f}",
+                    f"{stats[calibr_name]['f1_max']:.2f}",
+                    f"{stats[calibr_name]['f1_mean']:.2f}",
+                    f"{stats[calibr_name]['error_clfy']:.2f}",
+                    f"{stats[calibr_name]['error_min']:.2f}",
+                    f"{stats[calibr_name]['error_max']:.2f}",
+                    f"{stats[calibr_name]['error_mean']:.2f}",
                 ))
             table.append(NoEscape(r'\bottomrule'))
 
     doc.generate_tex(output_path)
-    print(f"LaTeX report generated at {output_path}.tex")
+    print(f"LaTeX table saved to {output_path}.tex")
 
 
 if __name__ == "__main__":
